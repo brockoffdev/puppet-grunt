8a5a3bfbcb6cc5222e7fa348350f2499572007d5
Bryant Rockoff <brockoffdev@gmail.com>
2014-07-17 11:25:17 -0400

    changes to init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,8 @@
 # Examples
 #
 #   include coda2
-class coda2 {
-  package { 'Coda2':
+class grunt {
+  package { 'Grunt':
     provider => 'zip',
     source   => 'http://download.panic.com/coda/Coda%202.0.14.zip'
   }
